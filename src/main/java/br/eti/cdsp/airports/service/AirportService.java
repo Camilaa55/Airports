8753a76884f7c834d965809f975324cb2311741c
Camilaa55 <paixaocamila277@gmail.com>
2025-11-27 09:39:42 -0300

    Término da Fase 2 –Implementado endpoint Airport -Add interface AirportRepository -Add classe AirportService -Add classe AirportController

---
+++ src/main/java/br/eti/cdsp/airports/service/AirportService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.eti.cdsp.airports.service;
+
+import br.eti.cdsp.airports.entities.Airport;
+import br.eti.cdsp.airports.entities.repositories.AirportRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author DIT2A
+ */
+@Service
+public class AirportService {
+    @Autowired
+    private AirportRepository airportRepository;
+    
+    public List<Airport> findAll() {
+        List<Airport> result = airportRepository.findAll();
+        return result;
+    }
+}

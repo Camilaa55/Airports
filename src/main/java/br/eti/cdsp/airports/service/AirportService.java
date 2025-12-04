634448b473b3394e80095a46d753449dd83b8dcc
Camilaa55 <paixaocamila277@gmail.com>
2025-12-04 09:30:42 -0300

    Término da Fase 5 –Update interface AirportRepository – Adicionado assinatura de método Airport findByIataCode(). -Update classe AirportService -Adicionado método findByIataCodeSearch. -Update classe AirportController – Adicionado endpoint iatacode/{iataCode}.

--- src/main/java/br/eti/cdsp/airports/service/AirportService.java
+++ src/main/java/br/eti/cdsp/airports/service/AirportService.java
@@ -36,4 +36,8 @@ public class AirportService {
                 .map(x -> new AirportMinDTO(x)).toList();
         return resultDTO;
     }
+    public Airport findByIataCode(String iataCode) {
+        Airport result = airportRepository.findByIataCode(iataCode);
+        return result;
+    }
 }
